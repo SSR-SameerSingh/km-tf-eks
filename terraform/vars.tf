2a881d1c2e38427cfe8929065f58b2a5da4d3413
krao-test <56043566+krao-test@users.noreply.github.com>
2022-08-29 22:07:54 -0700

    Create vars.tf

---
+++ terraform/vars.tf
@@ -0,0 +1,12 @@
+variable "AWS_REGION" {
+    default = "us-west-2"
+}
+
+variable "cluster_name" {}
+variable "project_name" {}
+variable "blueprint" {}
+variable "blueprint_version" {}
+variable "cloud_credential" {}
+variable "aws_region" {}
+variable "k8s_version" {}
+variable "instance_type" {}
